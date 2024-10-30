87535e1f011972b32457d298d6df31d8520091b0
yanirp <yanirp@mgarisr.onmicrosoft.com>
2024-10-30 23:54:47 +0200

    add allure reports and clean page object some mess

--- page_objects/authority_page.py
+++ page_objects/authority_page.py
@@ -4,8 +4,6 @@ class AuthorityPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
         self.standing_order_icon_btn = page.locator("//div[contains(text(),'הצטרפות להוראת קבע באשראי')]")
-        # frame = self.page.frame_locator("iframe[src*='SearchPay']")
-        # self.layla = frame.locator("#some-button")
 
     def click_standing_order_icon_btn(self):
         self.standing_order_icon_btn.click()
