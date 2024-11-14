46285b8ccf491ff79f8e7e1ba9441b8194f11312
yanirp <97969800+yanirp@users.noreply.github.com>
2024-11-14 10:50:22 +0200

    Revert "Presentaion2"

--- page_objects/payment_stub_page.py
+++ page_objects/payment_stub_page.py
@@ -1,9 +1,11 @@
-from models import payment_stub_details
+
+
+from page_objects.base_page import BasePage
 from page_objects.search_payment_stub_page import SearchPaymentStubPage
 
 
 class PaymentStubPage(SearchPaymentStubPage):
     def __init__(self, page):
-        super().__init__(page, payment_stub_details)
-        self.frame = page.frame_locator("iframe[src*='SearchPay']")
+        super().__init__(page)
+
 
--- tests/test_authority_page.py
+++ tests/test_authority_page.py
@@ -1,13 +1,11 @@
 import time
-import allure
+
+import pytest
 from playwright.async_api import Page
-from models.user_details import user
+
 from page_objects.authority_page import AuthorityPage
 
-
-@allure.description("getting to authority page and do refresh for the page")
-@allure.title("Test authority page functionality")
+@pytest.mark.description("Test authority page functionality")
 def test_authority_page_functions(page: Page):
     authority_page = AuthorityPage(page)
-    authority_page.user_login_to_mast_with_asset(user.phone)
-    time.sleep(5)
+    page.reload()
