7334a5abd7b20c91fe698fd7fccf4934fb09861e
yanirp <itsjustbusiness987@gmail.com>
2025-05-17 22:42:44 +0300

    add CI- playwright branch to setup CI

--- page_objects/authority_page.py
+++ page_objects/authority_page.py
@@ -6,7 +6,8 @@ from page_objects.base_page import BasePage
 class AuthorityPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
-        self.standing_order_icon_btn = page.locator("//div[contains(text(),'הצטרפות להוראת קבע באשראי')]")
+        #self.standing_order_icon_btn = page.locator("//div[contains(text(),'הצטרפות להוראת קבע באשראי')]")
+        self.standing_order_icon_btn = page.locator("//body/app-root[1]/app-main-nav[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/div[1]/app-services[1]/app-page-wraper[1]/div[1]/app-middle-side[1]/app-right-side[1]/app-widgets-wraper[1]/div[1]/div[1]/div[1]/app-container-widget[1]/div[1]/div[2]/div[1]/div[3]/img[1]")
         self.login_btn = page.locator("//button[contains(text(),'התחברות')]")
         self.login_input = page.locator("//input[@data-placeholder='מספר נייד']")
         self.otp_input = page.locator("//input[@data-placeholder='הזן כאן את הקוד']")
